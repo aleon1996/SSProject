0ed311d2cd0af83d55dccb26fd00a9009a8e9f00
Elisabetta Caldesi <elisabettacaldesi@Elisabettas-MacBook-Air.local>
2019-03-14 22:27:01 +0100

    changes to getNewReleases + getTweets

--- getNewReleases.py
+++ getNewReleases.py
@@ -3,26 +3,29 @@ import spotipy
 import sys
 import pprint
 import spotipy.util as util
-
+import codecs
 import os
-from json.decoder import JSONDecodeError
+
+#from json.decoder import JSONDecodeError
+
+SPOTIPY_CLIENT_ID='5b8dbcb9ff8f4fbcb3e62f0ae1f0b138'
+SPOTIPY_CLIENT_SECRET=''
+SPOTIPY_REDIRECT_URI='http://0.0.0.0:8000/' # run: python3 -m http.server 8000
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
 else:
     print("Whoops, need your username!")
-    print("usage: python new_releases.py [username]")
+    print("usage: python getNewReleases.py [username]")
     sys.exit()
 
-scope = 'user-top-read'
-# try:
-token = util.prompt_for_user_token(username, scope)
+scope = 'playlist-read-private'
+token = util.prompt_for_user_token(username,scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
 # except (AttributeError, JSONDecodeError):
 # os.remove(f".cache-{username}")
 # token = util.prompt_for_user_token(username, scope)
 
 if token:
-    #spotify:user:spotify:playlist:37i9dQZF1DX4JAvHpjipBk
     sp = spotipy.Spotify(auth=token)
     username = "spotify"
     playlist = "37i9dQZF1DX4JAvHpjipBk"
@@ -36,8 +39,13 @@ if token:
             artists.append(tracks[playlist_element]['track']['artists'][i]['name'])
         track_artist[song] = artists
 
+    f = codecs.open('newMusicFriday.txt', encoding='utf-8', mode='w')
     for k,v in track_artist.items():
-        print('Song: '+str(k)+ ' Artist: '+ ', '.join(v))
+        f.write(k)
+        f.write('"')
+        f.write(', '.join(v))
+        f.write('\n')
+    f.close()
     # sp = spotipy.Spotify(auth=token)
     #
     # response = sp.new_releases()
