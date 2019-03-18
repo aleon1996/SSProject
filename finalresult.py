d83d23f66079fd6afeb17548b94aeb6b861044f8
Elisabetta Caldesi <elisabettacaldesi@Elisabettas-MacBook-Air.local>
2019-03-17 20:24:00 -0400

    update

--- finalresult.py
+++ finalresult.py
@@ -5,10 +5,12 @@ import time
 import csv
 import json
 import tweepy
-from sentiments import clean_tweet, get_tweet_sentiment
-from regex import filter_tweet
 
 with open('data.json', 'r') as f:
     d = json.load(f)
 s = sum(d['1']['sentiment'])/len(d['1']['sentiment'])
+number = d['1']['number']
+time = d['1']['time']
 print(s*100)
+print(number)
+print(time)
